444e0344cb76d0147f8927d1c638216d25d62120
mpostaire <maxime_postaire@hotmail.fr>
2019-02-14 00:30:45 +0100

    fixed install script

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+cd ~/dotfiles
+stow */
 
-cd ~
-stow *
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
