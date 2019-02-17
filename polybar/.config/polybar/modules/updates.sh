62e7297f1a1fb1a98c6cbad7b23cb3353f8cf0f5
mpostaire <maxime_postaire@hotmail.fr>
2019-02-17 12:35:17 +0100

    aur support for update module + left click on it updates system

--- polybar/.config/polybar/modules/updates.sh
+++ polybar/.config/polybar/modules/updates.sh
@@ -3,16 +3,19 @@
 # Icons
 BAR_ICON=""
 
-get_total_updates() { UPDATES=$(checkupdates 2>/dev/null | wc -l); }
+UPDATES=$(checkupdates 2>/dev/null | wc -l)
+UPDATES_AUR=$(yay -Qum 2> /dev/null | wc -l)
 
-while true; do
-    get_total_updates
-    if (( UPDATES == 0 )); then
-        echo
-        sleep 1800
-    else
+if [[ $UPDATES == 0 && $UPDATES_AUR == 0 ]]; then
+    echo
+else
+    yellow=$(xrdb -query | grep "color6" | head -n1 | awk '{print $NF}')
+    if [[ $UPDATES == 0 ]]; then
+        echo "$BAR_ICON %{F${yellow}}$UPDATES_AUR%{F-}"
+    elif [[ $UPDATES_AUR == 0 ]]; then
         echo "$BAR_ICON $UPDATES"
-        sleep 60
+    else
+        echo "$BAR_ICON $UPDATES%{F${yellow}}+$UPDATES_AUR%{F-}"
     fi
-done
+fi
 
