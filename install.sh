e32c5f0dd0ed06b96cfd81f13efa4f28e80ab77a
mpostaire <maxime_postaire@hotmail.fr>
2019-03-04 12:44:01 +0100

    networkmanager_dmenu added as a dependency

--- install.sh
+++ install.sh
@@ -4,3 +4,7 @@ stow */
 touch ~/.config/mpd/pid ~/.config/mpd/state
 
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+yay -S networkmanager-dmenu-git
+
+# see https://wiki.archlinux.org/index.php/acpid
+systemctl start acpid.service
