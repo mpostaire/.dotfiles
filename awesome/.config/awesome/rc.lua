d5ea49291cd735d47b1b6d00d3246c82f14065aa
mpostaire <maxime_postaire@hotmail.fr>
2019-08-28 18:56:37 +0200

    fixed locale

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -1,3 +1,6 @@
+-- set locale
+os.setlocale('fr_FR.utf8')
+
 -- If LuaRocks is installed, make sure that packages installed through it are
 -- found (e.g. lgi). If LuaRocks is not installed, do nothing.
 pcall(require, "luarocks.loader")
