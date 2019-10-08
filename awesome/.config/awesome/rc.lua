721594062bb92ec42ef6546e0df1c4fbfff8b4d5
mpostaire <maxime_postaire@hotmail.fr>
2019-10-08 18:16:30 +0200

    moved back error handling in rc.lua

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -5,10 +5,34 @@ os.setlocale('fr_FR.utf8')
 -- found (e.g. lgi). If LuaRocks is not installed, do nothing.
 pcall(require, "luarocks.loader")
 
-require("awful.autofocus") -- when a client is closed, another client will be focused
+local naughty = require("naughty")
+local capi = {awesome = awesome}
 
--- error handling
-require("util.error_handling")
+-- Check if awesome encountered an error during startup and fell back to
+-- another config (This code will only ever execute for the fallback config)
+if capi.awesome.startup_errors then
+    naughty.notify({ preset = naughty.config.presets.critical,
+                     title = "Oops, there were errors during startup!",
+                     text = capi.awesome.startup_errors })
+end
+
+-- Handle runtime errors after startup
+do
+    local in_error = false
+    capi.awesome.connect_signal("debug::error", function (err)
+        -- Make sure we don't go into an endless error loop
+        if in_error then return end
+        in_error = true
+
+        naughty.notify({ preset = naughty.config.presets.critical,
+                         title = "Oops, an error happened!",
+                         text = tostring(err) })
+        in_error = false
+    end)
+end
+
+-- when a client is closed, another client will be focused
+require("awful.autofocus")
 
 -- Themes define colours, icons, font and wallpapers.
 -- do not place this line below
