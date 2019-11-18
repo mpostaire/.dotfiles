57954d04c8cdc4893ec0c0f7699179d14dd9089a
mpostaire <maxime_postaire@hotmail.fr>
2019-11-18 22:59:18 +0100

    taglist hover colors

--- awesome/.config/awesome/widgets/taglist.lua
+++ awesome/.config/awesome/widgets/taglist.lua
@@ -3,43 +3,86 @@ local beautiful = require("beautiful")
 local wibox = require("wibox")
 local gears = require("gears")
 local variables = require("config.variables")
+local color = require("util.color")
+local capi = {client = client, mouse = mouse}
 
 -- tags buttons widget mouse handling
 local taglist_buttons = gears.table.join(
     awful.button({ }, 1, function(t) t:view_only() end),
     awful.button({ variables.modkey }, 1, function(t)
-        if client.focus then
-            client.focus:move_to_tag(t)
+        if capi.client.focus then
+            capi.client.focus:move_to_tag(t)
         end
     end),
     awful.button({ }, 3, awful.tag.viewtoggle),
     awful.button({ variables.modkey }, 3, function(t)
-        if client.focus then
-            client.focus:toggle_tag(t)
+        if capi.client.focus then
+            capi.client.focus:toggle_tag(t)
         end
     end),
     awful.button({ }, 4, function(t) awful.tag.viewnext(t.screen) end),
     awful.button({ }, 5, function(t) awful.tag.viewprev(t.screen) end)
 )
 
+local icon_colors = {
+    mouse_hovering = {
+        selected = {
+            outer_circle = color.lighten_by(beautiful.taglist_fg_focus, 0.25),
+            inner_circle = color.lighten_by(beautiful.taglist_fg_focus, 0.25)
+        },
+        empty = {
+            outer_circle = color.lighten_by(beautiful.taglist_fg_empty, 0.5),
+            inner_circle = beautiful.taglist_bg_empty
+        },
+        urgent = {
+            outer_circle = color.lighten_by(beautiful.taglist_fg_urgent, 0.5),
+            inner_circle = color.lighten_by(beautiful.taglist_fg_urgent, 0.5)
+        },
+        occupied = {
+            outer_circle = color.lighten_by(beautiful.taglist_fg_occupied, 0.5),
+            inner_circle = color.lighten_by(beautiful.taglist_fg_occupied, 0.5)
+        }
+    },
+    normal = {
+        selected = {
+            outer_circle = beautiful.taglist_fg_focus,
+            inner_circle = beautiful.taglist_fg_focus
+        },
+        empty = {
+            outer_circle = beautiful.taglist_fg_empty,
+            inner_circle = beautiful.taglist_bg_empty
+        },
+        urgent = {
+            outer_circle = beautiful.taglist_fg_urgent,
+            inner_circle = beautiful.taglist_fg_urgent
+        },
+        occupied = {
+            outer_circle = beautiful.taglist_fg_occupied,
+            inner_circle = beautiful.taglist_fg_occupied
+        }
+    }
+}
 
 local function make_taglist_icons(widget, tag, index, tags)
     local outer_circle = widget:get_children_by_id('outer_circle')[1]
     local inner_circle = widget:get_children_by_id('inner_circle')[1]
 
+    local key = widget.mouse_hovering and "mouse_hovering" or "normal"
+
     if tag.selected then -- if tag selected
-        outer_circle.bg = beautiful.taglist_fg_focus
-        inner_circle.bg = beautiful.taglist_fg_focus
+        outer_circle.bg = icon_colors[key].selected.outer_circle
+        inner_circle.bg = icon_colors[key].selected.inner_circle
     elseif #tag:clients() == 0 then -- if tag empty
-        outer_circle.bg = beautiful.taglist_fg_empty
-        inner_circle.bg = beautiful.taglist_bg_empty
+        outer_circle.bg = icon_colors[key].empty.outer_circle
+        inner_circle.bg = icon_colors[key].empty.inner_circle
     elseif tag.urgent then -- if tag urgent
-        outer_circle.bg = beautiful.taglist_fg_urgent
-        inner_circle.bg = beautiful.taglist_fg_urgent
+        outer_circle.bg = icon_colors[key].urgent.outer_circle
+        inner_circle.bg = icon_colors[key].urgent.inner_circle
     else -- if tag occupied
-        outer_circle.bg = beautiful.taglist_fg_occupied
-        inner_circle.bg = beautiful.taglist_fg_occupied
+        outer_circle.bg = icon_colors[key].occupied.outer_circle
+        inner_circle.bg = icon_colors[key].occupied.inner_circle
     end
+
 end
 
 -- Place a widget for each screen
@@ -73,10 +116,13 @@ awful.screen.connect_for_each_screen(function(s)
             widget = wibox.container.margin,
             create_callback = function(self, tag, index, tags)
                 local old_cursor, old_wibox
+                self.mouse_hovering = false
                 self:connect_signal("mouse::enter", function()
-                    local w = mouse.current_wibox
+                    local w = capi.mouse.current_wibox
                     old_cursor, old_wibox = w.cursor, w
-                    w.cursor = "hand1"
+                    w.cursor = "hand2"
+                    self.mouse_hovering = true
+                    make_taglist_icons(self, tag, index, tags)
                 end)
 
                 self:connect_signal("mouse::leave", function()
@@ -84,6 +130,8 @@ awful.screen.connect_for_each_screen(function(s)
                         old_wibox.cursor = old_cursor
                         old_wibox = nil
                     end
+                    self.mouse_hovering = false
+                    make_taglist_icons(self, tag, index, tags)
                 end)
 
                 make_taglist_icons(self, tag, index, tags)
