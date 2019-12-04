01fc5d28d790c3ab2355d5624c3b6dee9724d91c
mpostaire <maxime_postaire@hotmail.fr>
2019-12-04 19:17:53 +0100

    removed old power menu bis

--- awesome/.config/awesome/util/rofi.lua
+++ awesome/.config/awesome/util/rofi.lua
@@ -1,7 +1,6 @@
 -- replace this by an awesome implementation using popups later
 
 local spawn = require("awful.spawn")
-local capi = {mouse = mouse, awesome = awesome}
 
 local rofi = {}
 
@@ -9,29 +8,6 @@ local rofi = {}
 function rofi.launcher_menu(mode)
     local cmd = "rofi -modi drun,window -show " .. mode .. " -theme launchermenu"
     spawn.easy_async(cmd, function() end)
-end
-
--- deprecated
-function rofi.power_menu()
-    local opts = " Verrouiller\n Déconnexion\n Mettre en veille\n Redémarrer\n Éteindre"
-    local cmd = "echo -e \"" ..opts.. "\" | rofi -dmenu -i -theme powermenu -p 'Système'"
-
-    spawn.easy_async_with_shell(cmd,
-        function(stdout)
-            local output = string.sub(stdout, 5, -2)
-            if output == "Verrouiller" then
-                spawn.easy_async_with_shell("~/.scripts/lock.sh", function() end)
-            elseif output == "Déconnexion" then
-                capi.awesome.quit()
-            elseif output == "Mettre en veille" then
-                spawn.easy_async_with_shell("systemctl suspend", function() end)
-            elseif output == "Redémarrer" then
-                spawn.easy_async_with_shell("systemctl reboot", function() end)
-            elseif output == "Éteindre" then
-                spawn.easy_async_with_shell("systemctl -i poweroff", function() end)
-            end
-        end
-    )
 end
 
 -- TODO: replace with a popup widget ? (may be complicated)
