5f9ee71c6e401784a7ff1f69b5b511e57ef39ff9
mpostaire <maxime_postaire@hotmail.fr>
2019-12-07 00:13:44 +0100

    removed useless rofi.lua function

--- awesome/.config/awesome/util/rofi.lua
+++ awesome/.config/awesome/util/rofi.lua
@@ -4,12 +4,6 @@ local spawn = require("awful.spawn")
 
 local rofi = {}
 
--- mode = drun|window
-function rofi.launcher_menu(mode)
-    local cmd = "rofi -modi drun,window -show " .. mode .. " -theme launchermenu"
-    spawn.easy_async(cmd, function() end)
-end
-
 -- TODO: replace with a popup widget ? (may be complicated)
 function rofi.network_menu()
     spawn.easy_async("networkmanager_dmenu", function() end)
