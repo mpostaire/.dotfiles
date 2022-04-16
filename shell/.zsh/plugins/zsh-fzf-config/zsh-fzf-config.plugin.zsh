e0c5510150734be60a6432165dcb8c79a48f7b90
mpostaire <maxime_postaire@hotmail.fr>
2022-04-17 00:34:26 +0200

    update outdated comment related to previous commit

--- shell/.zsh/plugins/zsh-fzf-config/zsh-fzf-config.plugin.zsh
+++ shell/.zsh/plugins/zsh-fzf-config/zsh-fzf-config.plugin.zsh
@@ -97,6 +97,6 @@ FZF_TMUX_HEIGHT=70%
 
 # overwrite -ftb-colorize function from fzf-tab to fix symlinks targets not properly colored
 # also overwrites -ftb-fzf function from fzf-tab to allow custom preview window when the completion
-# list are files/directories as well as new variable buffer to get the user input in the zsh (not fzf) prompt
+# list contains files and/or directories.
 # TODO: open PR to merge the fix instead of overwriting this
 fpath+=(${0:A:h}/functions)
